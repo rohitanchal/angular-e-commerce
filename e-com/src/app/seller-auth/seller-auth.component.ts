ed28cc6dac7475877a9ae66ec80ba4582b8f937e
Rohit Kumar Anchal <105239963+rohitanchal@users.noreply.github.com>
2023-10-23 03:03:29 +0530

    add services for seller

--- e-com/src/app/seller-auth/seller-auth.component.ts
+++ e-com/src/app/seller-auth/seller-auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SellerService } from '../services/seller.service';
 import {Router} from '@angular/router';
+import { signUp } from '../data-type';
 
 @Component({
   selector: 'app-seller-auth',
@@ -10,7 +11,7 @@ import {Router} from '@angular/router';
 export class SellerAuthComponent {
   constructor(private seller:SellerService, private router:Router){}
 
-  signUp(data:object):void{
+  signUp(data:signUp):void{
 
     this.seller.userSignUp(data).subscribe((result)=>{
       if(result){
